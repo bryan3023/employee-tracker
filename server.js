2b9a76f3af0ab2628744fc507d53ee84989b3bb0
Bryan <bryan.fennell@hotmail.com>
2020-05-09 13:39:43 -0400

    Started EmployeeDatabase class, set up preliminary menu.

--- server.js
+++ server.js
@@ -1,18 +1,57 @@
 const
-  mysql = require("mysql"),
-  inquirer = require("inquirer");
+  EmployeeDatabase = require("./lib/EmployeeDatabase"),
+  inquirer = require("inquirer"),
+  consoleTable = require("console.table")
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "bryan3023.employee_tracker",
-  password: "1L5i%KV&^@jc7LFe5c5C",
-  database: "employee_tracker_DB"
-});
+const
+  database = new EmployeeDatabase();
+
+function init() {
+  promptMainMenu()
+}
+
+function promptMainMenu() {
+  const choices = mainMenuLookup.map(m => m.menuItem)
+
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: choices,
+      name: "choice"
+    }
+  ]).then(({choice}) => {
+    const action = mainMenuLookup
+      .filter(m => m.menuItem === choice)[0]
+      .action
+
+    action()
+  })
+}
 
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log(`connected as id ${connection.threadId}\n`);
-  //createProduct();
-});
+const mainMenuLookup = [
+  {
+    menuItem: "View All Employees",
+    action: viewAllEmployees
+  },
+  {
+    menuItem: "Exit",
+    action: quit
+  }
+]
+
+function viewAllEmployees() {
+  database.getFullEmployeeListing()
+    .then(result => {
+      console.table(result)
+      promptMainMenu()
+    })
+}
+
+
+function quit() {
+  database.close()
+}
+
+init()
