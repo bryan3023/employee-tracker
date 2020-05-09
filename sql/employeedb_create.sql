741272855cf6bd0293a1f54caa429b8986c77b05
Bryan <bryan.fennell@hotmail.com>
2020-05-08 22:51:11 -0400

    Made progress on SQL database creation and seed scripts.

--- sql/employeedb_create.sql
+++ sql/employeedb_create.sql
@@ -1,4 +1,91 @@
-DROP DATABASE IF EXISTS employee_trackere_DB;
-CREATE DATABASE employee_trackere_DB;
+DROP DATABASE IF EXISTS employee_tracker_DB;
+CREATE DATABASE employee_tracker_DB;
 
-USE employee_trackere_DB;
+USE employee_tracker_DB;
+
+
+CREATE TABLE department (
+  id
+    INT
+    NOT NULL
+    AUTO_INCREMENT
+    PRIMARY KEY,
+
+  name
+    VARCHAR(30)
+    NOT NULL
+);
+
+
+CREATE TABLE role (
+  id
+    INT
+    NOT NULL
+    AUTO_INCREMENT
+    PRIMARY KEY,
+
+  title
+    VARCHAR(30)
+    NOT NULL,
+
+  salary
+    DECIMAL(8,2)
+    NOT NULL,
+
+  department_id
+    INT
+    NOT NULL,
+
+  CONSTRAINT FK_department
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+
+CREATE TABLE employee (
+  id
+    INT
+    NOT NULL
+    AUTO_INCREMENT
+    PRIMARY KEY,
+
+  given_name
+    VARCHAR(30)
+    NOT NULL,
+
+  surname
+    VARCHAR(30)
+    NOT NULL,
+
+  role_id
+    INT
+    NOT NULL,
+  
+  manager_id
+    INT
+    NULL,
+
+  CONSTRAINT FK_role
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+
+  CONSTRAINT FK_manager
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+);
+
+
+CREATE VIEW employee_summary AS
+  SELECT
+    e.id AS ID,
+    e.given_name AS 'Given Name',
+    e.surname AS Surname,
+    r.title AS Title,
+    d.name AS Department,
+    r.salary AS Salary,
+    CONCAT(m.given_name, ' ', m.surname) AS Manager
+  FROM
+    employee AS e
+    INNER JOIN role AS r ON e.id = r.id
+    INNER JOIN department AS d ON d.id = r.id
+    LEFT OUTER JOIN employee AS m ON e.manager_id = m.id
---
+++ sql/employeedb_seed.sql
@@ -0,0 +1,75 @@
+USE employee_tracker_DB;
+
+INSERT INTO department
+  (name)
+VALUES
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
+  ('Legal')
+;
+
+SELECT @salesID := id FROM department WHERE name = 'Sales';
+SELECT @engineeringID := id FROM department WHERE name = 'Engineering';
+SELECT @financeID := id FROM department WHERE name = 'Finance';
+SELECT @legalID := id FROM department WHERE name = 'Legal';
+
+INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Sales Lead', 100000, @salesID),
+  ('Salesperson', 75000, @salesID),
+  ('Lead Engineer', 150000, @engineeringID),
+  ('Software Engineer', 120000, @engineeringID),
+  ('Accountant', 125000, @financeID),
+  ('Legal Team Lead', 250000, @legalID),
+  ('Lawyer', 150000, @legalID)
+;
+
+
+SELECT @salesLeadID := id FROM role WHERE title = 'Sales Lead';
+SELECT @salespersonID := id FROM role WHERE title = 'Salesperson';
+SELECT @leadEngineeerID := id FROM role WHERE title = 'Lead Engineer';
+SELECT @softwareEngineerID := id FROM role WHERE title = 'Software Engineer';
+SELECT @accountantID := id FROM role WHERE title = 'Accountant';
+SELECT @LegalTeamLeadID := id FROM role WHERE title = 'Legal Team Lead';
+SELECT @lawyerID := id FROM role WHERE title = 'Lawyer';
+
+INSERT INTO employee
+  (given_name, surname, role_id)
+VALUES
+  ('John', 'Doe', @salesLeadID),
+  ('Mike', 'Chan', @salespersonID),
+  ('Ashley', 'Rodriguez', @leadEngineeerID),
+  ('Kevin', 'Tupik', @softwareEngineerID),
+  ('Malia', 'Brown', @accountantID),
+  ('Sarah', 'Lourd', @LegalTeamLeadID),
+  ('Tom', 'Allen', @lawyerID)
+;
+
+SELECT @arodriguezID := id FROM employee WHERE surname = 'Rodriguez';
+SELECT @jdoeID := id FROM employee WHERE surname = 'Doe';
+SELECT @slourdID := id FROM employee WHERE surname = 'lourd';
+SELECT @mchanID := id FROM employee WHERE surname = 'Chan';
+
+UPDATE employee
+SET
+  manager_id = @arodriguezID
+WHERE
+  surname = 'Doe'
+  OR surname = 'Tupik';
+
+UPDATE employee
+SET
+  manager_id = @jdoeID
+WHERE
+  surname = 'Chan';
+
+UPDATE employee
+SET
+  manager_id = @slourdID
+WHERE
+  surname = 'Allen';
+
+select * from employee_summary;
+select * from employee;
